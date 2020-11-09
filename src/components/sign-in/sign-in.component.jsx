4efb296a63704bcfefaf46159186f27527d65370
abdul-ashour <abdul.ashour@capablue.com>
2020-11-09 20:02:47 +0000

    Setup Redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,7 +6,7 @@ import FormInput from "../form-input/form-input.component";
 
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils"
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils"
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -19,10 +19,18 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+        const { email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+        }
+        catch(e) {
+            console.error(e);
+        }
 
-        this.setState({email: '', password: ''});
+
     }
 
     handleChange = event => {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,6 +19,32 @@ export const auth = firebase.auth();
 
 export const firestore = firebase.firestore();
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      })
+    }
+    catch(e){
+      console.error('Error creating user', e.message);
+    }
+  }
+
+  return userRef;
+}
+
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 
